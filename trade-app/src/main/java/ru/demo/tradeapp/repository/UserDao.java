058b58d65c8d9866656ae625ef9302e747c8d1f3
Aleksandr <116497492+impos21sible@users.noreply.github.com>
2024-10-17 11:12:43 +0300

    Add files via upload

---
+++ trade-app/src/main/java/ru/demo/tradeapp/repository/UserDao.java
@@ -0,0 +1,16 @@
+package ru.demo.tradeapp.repository;
+
+
+
+
+import ru.demo.tradeapp.model.User;
+
+
+public class UserDao extends BaseDao<User> {
+    public UserDao(){
+        super(User.class);
+    }
+
+
+
+}
